ff35b3fbf517048c30dea8c5b871ae7c56ab0501
SolohagaLukas <solohagalukas@gmail.com>
2022-04-10 19:18:22 -0300

    Express Middleware de Error

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,15 +1,15 @@
 const pool = require('../routes/db');
 
-const getAllTasks = async (req, res) => {
+const getAllTasks = async (req, res, next) => {
     try {
         const allTasks = await pool.query('SELECT * FROM task');
         res.json(allTasks.rows)
     } catch (error) {
-        console.log(error.message);
+        next(error);
     }
 }
 
-const getTask = async (req, res) => {
+const getTask = async (req, res, next) => {
     try {
         const {id} = req.params
         const result = await pool.query('SELECT * FROM task WHERE id = $1', [id])
@@ -22,11 +22,11 @@ const getTask = async (req, res) => {
 
         res.json(result.rows[0]);
     } catch (error) {
-        console.log(error.message);
+        next(error);
     }
 }
 
-const createTask = async (req, res) => {
+const createTask = async (req, res, next) => {
     const { title, description } = req.body
 
     try {
@@ -34,35 +34,42 @@ const createTask = async (req, res) => {
 
         res.json(result.rows[0]);
     } catch (error) {
-        console.log(error.message);
-        res.json({ error: error.message });
+        next(error);
     }
 }
 
-const deleteTask = async (req, res) => {
+const deleteTask = async (req, res, next) => {
     const {id} = req.params;
 
-    const result = await pool.query('DELETE FROM task WHERE id = $1', [id]);
+    try {
+        const result = await pool.query('DELETE FROM task WHERE id = $1', [id]);
 
-    if (result.rowCount === 0) return res.status(404).json({
-        message: "Task not found"
-    });
+        if (result.rowCount === 0) return res.status(404).json({
+            message: "Task not found"
+        });
 
-    return res.sendStatus(204);
-}
+        return res.sendStatus(204);
+    } catch (error) {
+        next(error);
+    }  
+};
 
-const updateTask = async (req, res) => {
-    const { id } = req.params;
-    const { title, description } = req.body;
+const updateTask = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { title, description } = req.body;
 
-    const result = await pool.query('UPDATE task SET title = $1, description = $2 WHERE id = $3 RETURNING *', 
-    [title, description, id]);
+        const result = await pool.query('UPDATE task SET title = $1, description = $2 WHERE id = $3 RETURNING *', 
+        [title, description, id]);
 
-    if (result.rows.length === 0) return res.status(404).json({
-        message: "Task not found"
-    });
+        if (result.rows.length === 0) return res.status(404).json({
+            message: "Task not found"
+        });
 
-    return res.json(result.rows[0])
+        return res.json(result.rows[0])
+    } catch (error) {
+        next(error);
+    }
 }
 
 module.exports = {
