d68f31c4c310cbafb60d5db16fce95a4b688014c
SolohagaLukas <solohagalukas@gmail.com>
2022-04-10 01:05:13 -0300

    CRUD

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,17 +1,74 @@
+const pool = require('../routes/db');
+
 const getAllTasks = async (req, res) => {
-    res.send('returning a list of tasks');
+    try {
+        const allTasks = await pool.query('SELECT * FROM task');
+        res.json(allTasks.rows)
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
-const getTask = (req, res) => {
-    res.send('returning a single task');
+const getTask = async (req, res) => {
+    try {
+        const {id} = req.params
+        const result = await pool.query('SELECT * FROM task WHERE id = $1', [id])
+
+        console.log(result);
+
+        if (result.rows.length === 0) return res.status(404).json({
+            message: 'Task not found'
+        })
+
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
-const createTask = (req, res) => {
-    res.send('creating new task');
+const createTask = async (req, res) => {
+    const { title, description } = req.body
+
+    try {
+        const result = await pool.query("INSERT INTO task (title, description) VALUES ($1, $2) RETURNING *", [title, description]);
+
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.log(error.message);
+        res.json({ error: error.message });
+    }
+}
+
+const deleteTask = async (req, res) => {
+    const {id} = req.params;
+
+    const result = await pool.query('DELETE FROM task WHERE id = $1', [id]);
+
+    if (result.rowCount === 0) return res.status(404).json({
+        message: "Task not found"
+    });
+
+    return res.sendStatus(204);
+}
+
+const updateTask = async (req, res) => {
+    const { id } = req.params;
+    const { title, description } = req.body;
+
+    const result = await pool.query('UPDATE task SET title = $1, description = $2 WHERE id = $3 RETURNING *', 
+    [title, description, id]);
+
+    if (result.rows.length === 0) return res.status(404).json({
+        message: "Task not found"
+    });
+
+    return res.json(result.rows[0])
 }
 
 module.exports = {
     getAllTasks,
     getTask,
-    createTask
+    createTask,
+    deleteTask,
+    updateTask
 }
